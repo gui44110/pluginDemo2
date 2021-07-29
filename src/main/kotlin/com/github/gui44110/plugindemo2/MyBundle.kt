d8a5c97c78690d81c0df728c92f47ac0e19ce156
GitHub Action <action@github.com>
2021-07-29 08:30:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/gui44110/plugindemo2/MyBundle.kt
+++ src/main/kotlin/com/github/gui44110/plugindemo2/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gui44110.plugindemo2
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gui44110/plugindemo2/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/gui44110/plugindemo2/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gui44110.plugindemo2.listeners
 
+import com.github.gui44110.plugindemo2.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
